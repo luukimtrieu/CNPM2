c6a3e729102b4f4fee271d872a4fdc4a0bf451df
Hieu <anrayno200@gmail.com>
2022-06-01 20:54:11 +0700

    updated database

--- app/src/main/java/com/example/cnpm/HomeActivity.java
+++ app/src/main/java/com/example/cnpm/HomeActivity.java
@@ -5,17 +5,15 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
+import com.example.cnpm.Database.AppDataBase;
+import com.example.cnpm.Database.Employee;
+import com.example.cnpm.Database.HR;
 import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -65,7 +63,8 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        AppDataBase dataBase = new AppDataBase(this, "database.db", null, 1);
+        AppDataBase dataBase = new AppDataBase(this, "database5.db", null, 1);
+
     }
 
 
